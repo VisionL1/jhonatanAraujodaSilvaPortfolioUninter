7194660168b2aabdd8858e41122ed37c2e0db1d1
Jhonatan Araujo <111452534+VisionL1@users.noreply.github.com>
2024-09-02 08:05:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ links.forEach((link, i) => {
 
 //Sistema para esconder ou revelar o menu utilizando o scroll do mouse
 var scrollMouse;
-navbar = document.getElementById('.navbar');
+navbar = document.getElementById('navbar');
 window.addEventListener('scroll', function(){
     // toda vez que usar o scroll do mouse essa função será chamada
-var scrollUp = window.pageYOffset || document.documentElement.scrollTop;
+var scrollUp = window.pageYOffset || document.documentElement.scrollUp;
 if(scrollUp > scrollMouse) {
     navbar.style.top='-80px';
 }
